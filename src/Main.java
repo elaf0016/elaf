f1b2c5c18702c162069e8294a638bc6222f3be2d
elaf0016 <elal0001@stud.kea.dk>
2024-03-18 14:20:06 +0100

    ny

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String name= "elaf";
+       int age= 27;
+        System.out.println("hej");
+
+
+    }
+}
